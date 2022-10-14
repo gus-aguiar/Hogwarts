3e5301d92be5533c3b127b236f5376bc2d32d003
gus-aguiar <gustavopma90@gmail.com>
2022-10-14 18:32:34 -0300

    vamos ao lint

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// let botaoEntrar = document.getElementById('botaoEntrar')
-// botaoEntrar.addEventListener ('click' ,
-
-// function alertar () {
-//   let eMail = document.getElementById('form1Email').value;
-//   let senha = document.getElementById('form1Senha').value;
-//   console.log(eMail.lenght)
-//     if(eMail.lenght < 1 ){
-//         alert('Email ou senha inválidos');
-//     } else if (eMail.lenght !== null && senha.lenght !== null){
-//         alert('Olá, Tryber!');
-//     }
-// })
-
 let botaoEntrar = document.getElementById('botaoEntrar')
 botaoEntrar.addEventListener ('click' ,
 function validamail() {
