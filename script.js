0f695fe81a3ba9844850a78e798d8562a9be6507
gus-aguiar <gustavopma90@gmail.com>
2022-10-14 18:48:52 -0300

    lint feito

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-let botaoEntrar = document.getElementById('botaoEntrar')
-botaoEntrar.addEventListener ('click' ,
-function validamail() {
-    let eMail = document.getElementById('form1Email').value;
-    let senha = document.getElementById('form1Senha').value;
-    if (eMail == "tryber@teste.com" || senha == "123456") {
-        alert('Olá, Tryber!');
-    }else {alert('Email ou senha inválidos.')}
-  }
-)
+const botaoEntrar = document.getElementById('botaoEntrar');
+botaoEntrar.addEventListener('click',
+  () => {
+    const eMail = document.getElementById('form1Email').value;
+    const senha = document.getElementById('form1Senha').value;
+    if (eMail === 'tryber@teste.com' || senha === '123456') {
+      alert('Olá, Tryber!');
+    } else { alert('Email ou senha inválidos.'); }
+  });
 
-
-let enviar = document.getElementById("submit-btn");
-let checkB = document.getElementById("agreement");
+const enviar = document.getElementById('submit-btn');
+const checkB = document.getElementById('agreement');
 enviar.disabled = true;
-checkB.addEventListener("change", xablau);
-function xablau() {
-  if (document.getElementById("agreement").check === false) {
-    enviar.disabled = true; 
-  } else {
-    enviar.disabled = false;
-  }
-}
+checkB.addEventListener('change',
+  () => {
+    if (document.getElementById('agreement').check === false) {
+      enviar.disabled = true;
+    } else {
+      enviar.disabled = false;
+    }
+  });
