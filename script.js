8b9229f38731249a6ba83c7dd0cd2927a5730e06
gus-aguiar <gustavopma90@gmail.com>
2022-10-14 17:59:30 -0300

    fizemos a 3

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// let botaoEntrar = document.getElementById('botaoEntrar')
+// botaoEntrar.addEventListener ('click' ,
+
+// function alertar () {
+//   let eMail = document.getElementById('form1Email').value;
+//   let senha = document.getElementById('form1Senha').value;
+//   console.log(eMail.lenght)
+//     if(eMail.lenght < 1 ){
+//         alert('Email ou senha inválidos');
+//     } else if (eMail.lenght !== null && senha.lenght !== null){
+//         alert('Olá, Tryber!');
+//     }
+// })
+
 let botaoEntrar = document.getElementById('botaoEntrar')
 botaoEntrar.addEventListener ('click' ,
-
-function alertar () {
-  let eMail = document.getElementById('form1Email').value;
-  let senha = document.getElementById('form1Senha').value;
-  console.log(eMail.lenght)
-    if(eMail.lenght < 1 ){
-        alert('Email ou senha inválidos');
-    } else if (eMail.lenght !== null && senha.lenght !== null){
-        alert('Olá, Tryber!');
-    }
-})
+function validamail() {
+    let eMail = document.getElementById('form1Email').value;
+    let senha = document.getElementById('form1Senha').value;
+    if (eMail == "" || senha == "") {
+      alert("Email ou senha inválidos");
+    }else {alert('Olá, Tryber!')}
+  }
+)
